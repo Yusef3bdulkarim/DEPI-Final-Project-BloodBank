e15f312de7e4191b323edfc214e75b5b0926c934
andrew <andrewtimmy547@gmail.com>
2026-04-29 15:20:04 +0300

    Update gradle config

--- BloodLink3/app/build.gradle.kts
+++ BloodLink3/app/build.gradle.kts
@@ -48,7 +48,7 @@ dependencies {
     implementation(libs.androidx.compose.ui.graphics)
     implementation(libs.androidx.compose.ui.tooling.preview)
     implementation(libs.androidx.compose.material3)
-    implementation("androidx.navigation:navigation-compose:2.7.7")
+    implementation("androidx.navigation:navigation-compose:2.9.8")
     implementation(libs.androidx.ui)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
