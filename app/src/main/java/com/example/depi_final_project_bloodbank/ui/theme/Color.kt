9d88d18c652d9f5fcc19776734dd80e725ec4beb
Mohamed Ahmed Sabry <Muhammad2602095@fci.sohag.edu.eg>
2026-05-13 16:13:04 +0300

    finish theme file

--- app/src/main/java/com/example/depi_final_project_bloodbank/ui/theme/Color.kt
+++ app/src/main/java/com/example/depi_final_project_bloodbank/ui/theme/Color.kt
@@ -2,24 +2,14 @@ package com.example.depi_final_project_bloodbank.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val Purple80 = Color(0xFFD0BCFF)
-val PurpleGrey80 = Color(0xFFCCC2DC)
-val Pink80 = Color(0xFFEFB8C8)
-
-val Purple40 = Color(0xFF6650a4)
-val PurpleGrey40 = Color(0xFF625b71)
-val Pink40 = Color(0xFF7D5260)
-
 // Primary Palette
 val MaroonPrimary = Color(0xFF74121D)
 val MaroonOnPrimary = Color(0xFFFFFFFF)
 val MaroonContainer = Color(0xFFF9E8E8)
-
 // Secondary/Neutral
 val DarkNavy = Color(0xFF15141F)
 val GreyText = Color(0xFF9E9E9E)
 val BackgroundLight = Color(0xFFF8F9FA)
-
 // Status Colors
 val SuccessGreen = Color(0xFF2ECC71)
 val UrgentRed = Color(0xFFB00020)
