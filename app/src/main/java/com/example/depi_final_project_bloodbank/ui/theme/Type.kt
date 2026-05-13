8e8b25c21b10d503ade668c2e841707e5ebfc5b7
Mohamed Ahmed Sabry <Muhammad2602095@fci.sohag.edu.eg>
2026-05-13 15:48:24 +0300

    add font files and customize typography

--- app/src/main/java/com/example/depi_final_project_bloodbank/ui/theme/Type.kt
+++ app/src/main/java/com/example/depi_final_project_bloodbank/ui/theme/Type.kt
@@ -2,33 +2,45 @@ package com.example.depi_final_project_bloodbank.ui.theme
 
 import androidx.compose.material3.Typography
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
+import com.example.depi_final_project_bloodbank.R
 
-// Set of Material typography styles to start with
+val NotoSansArabic = FontFamily(
+    Font(R.font.noto_regular, FontWeight.Normal),
+    Font(R.font.noto_bold, FontWeight.Bold),
+    Font(R.font.noto_semibold, FontWeight.SemiBold),
+    Font(R.font.noto_light, FontWeight.Light)
+)
 val Typography = Typography(
-    bodyLarge = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Normal,
+    // العناوين الكبيرة - طلب تبرع عاجل، تم تبرعك
+    titleLarge = TextStyle(
+        fontFamily = NotoSansArabic,
+        fontWeight = FontWeight.Bold,
+        fontSize = 18.sp,
+        lineHeight = 26.sp,
+    ),
+    // عناوين متوسطة - التنبيهات، قائمة الطلبات
+    titleMedium = TextStyle(
+        fontFamily = NotoSansArabic,
+        fontWeight = FontWeight.SemiBold,
         fontSize = 16.sp,
         lineHeight = 24.sp,
-        letterSpacing = 0.5.sp
-    )
-    /* Other default text styles to override
-    titleLarge = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Normal,
-        fontSize = 22.sp,
-        lineHeight = 28.sp,
-        letterSpacing = 0.sp
     ),
+    // النصوص العادية - الرسائل، التفاصيل
+    bodyLarge = TextStyle(
+        fontFamily = NotoSansArabic,
+        fontWeight = FontWeight.Normal,
+        fontSize = 14.sp,
+        lineHeight = 22.sp,
+    ),
+    // النصوص الصغيرة - الوقت، الحالة
     labelSmall = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Medium,
-        fontSize = 11.sp,
+        fontFamily = NotoSansArabic,
+        fontWeight = FontWeight.Light,
+        fontSize = 12.sp,
         lineHeight = 16.sp,
-        letterSpacing = 0.5.sp
     )
-    */
 )
