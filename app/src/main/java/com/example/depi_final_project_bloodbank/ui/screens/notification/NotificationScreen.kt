48120ef16294a9acae2816770330a1f9a30166a9
Mohamed Ahmed Sabry <Muhammad2602095@fci.sohag.edu.eg>
2026-05-14 19:13:04 +0300

    add notification screen header

--- app/src/main/java/com/example/depi_final_project_bloodbank/ui/screens/notification/NotificationScreen.kt
+++ app/src/main/java/com/example/depi_final_project_bloodbank/ui/screens/notification/NotificationScreen.kt
@@ -1,11 +1,19 @@
 package com.example.depi_final_project_bloodbank.ui.screens.notification
 
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.statusBarsPadding
+import androidx.compose.foundation.layout.width
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.automirrored.filled.ArrowBack
+import androidx.compose.material.icons.filled.Notifications
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
@@ -13,7 +21,12 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 
 @Composable
 fun NotificationScreen(modifier: Modifier = Modifier) {
@@ -22,6 +35,7 @@ fun NotificationScreen(modifier: Modifier = Modifier) {
             .fillMaxSize()
     ) {
         NotificationsTopBar(onBackClick = {})
+        NotificationsHeader(count = 5 , onMarkAllRead = {})
     }
 }
 @OptIn(ExperimentalMaterial3Api::class)
@@ -44,4 +58,37 @@ fun NotificationsTopBar(onBackClick: () -> Unit) {
                 )
             }
         })
+}
+
+@Composable
+fun NotificationsHeader(count: Int, onMarkAllRead: () -> Unit) {
+    Row(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(horizontal = 16.dp, vertical = 12.dp),
+        horizontalArrangement = Arrangement.SpaceBetween,
+        verticalAlignment = Alignment.CenterVertically
+    ) {
+        Row(verticalAlignment = Alignment.CenterVertically) {
+            Icon(
+                imageVector = Icons.Default.Notifications,
+                contentDescription = null,
+                tint = MaterialTheme.colorScheme.primary,
+                modifier = Modifier.size(18.dp)
+            )
+            Spacer(modifier = Modifier.width(4.dp))
+            Text(
+                text = "You have $count new notifications",
+                style = MaterialTheme.typography.labelSmall,
+                fontSize = 14.sp,
+                color = MaterialTheme.colorScheme.primary
+            )
+        }
+        Text(
+            text = "Mark all as read",
+            style = MaterialTheme.typography.bodyLarge,
+            color = MaterialTheme.colorScheme.primary,
+            modifier = Modifier.clickable { onMarkAllRead() }
+        )
+    }
 }
