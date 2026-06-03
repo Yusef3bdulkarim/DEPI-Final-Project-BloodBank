f79cbf69f2017404168a0824b2b36599bc16f953
Mohamed Ahmed Sabry <Muhammad2602095@fci.sohag.edu.eg>
2026-06-03 20:34:45 +0300

    add donation model and status

---
+++ app/src/main/java/com/example/depi_final_project_bloodbank/domain/enums/DonationStatus.kt
@@ -0,0 +1,8 @@
+package com.example.depi_final_project_bloodbank.domain.enums
+
+
+enum class DonationStatus {
+    PENDING,     // وافق يتبرع
+    CONFIRMED,   // تم التبرع فعلاً
+    CANCELLED    // تراجع
+}
---
+++ app/src/main/java/com/example/depi_final_project_bloodbank/domain/model/Donation.kt
@@ -0,0 +1,12 @@
+package com.example.depi_final_project_bloodbank.domain.model
+
+import com.example.depi_final_project_bloodbank.domain.enums.DonationStatus
+
+data class Donation(
+    val id: String = "",
+    val donorId: String = "",
+    val requestId: String = "",
+    val status: DonationStatus = DonationStatus.PENDING,
+    val createdAt: Long = 0L,
+    val confirmedAt: Long? = null
+)
