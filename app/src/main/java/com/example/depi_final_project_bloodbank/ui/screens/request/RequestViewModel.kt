4084f3ae4f1cd006fc1d1c9f21029732b7ef49a6
single activity <nasreldinsayed377@gmail.com>
2026-06-05 18:49:40 +0300

    feat(request-data): create BloodRequest model and integrate location logic into ViewModel

--- app/src/main/java/com/example/depi_final_project_bloodbank/ui/screens/request/RequestViewModel.kt
+++ app/src/main/java/com/example/depi_final_project_bloodbank/ui/screens/request/RequestViewModel.kt
@@ -1,7 +1,11 @@
 package com.example.depi_final_project_bloodbank.ui.screens.request
 
+import android.annotation.SuppressLint
 import androidx.lifecycle.ViewModel
 import com.example.depi_final_project_bloodbank.domain.model.BloodRequest
+import com.google.android.gms.location.FusedLocationProviderClient
+import com.google.android.gms.location.Priority
+import com.google.android.gms.tasks.CancellationTokenSource
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.asStateFlow
 
@@ -12,9 +16,45 @@ class RequestViewModel : ViewModel() {
     private val _error = MutableStateFlow<String?>(null)
     val error = _error.asStateFlow()
 
+    // متغيرات الـ Location
+    private val _locationLoading = MutableStateFlow(false)
+    val locationLoading = _locationLoading.asStateFlow()
+
+    private val _locationSuccess = MutableStateFlow(false)
+    val locationSuccess = _locationSuccess.asStateFlow()
+
     fun updateRequest(req: BloodRequest) {
         _request.value = req
         _error.value = null
+    }
+
+    @SuppressLint("MissingPermission") // الصلاحيات متأمنة في شاشة الـ UI
+    fun fetchCurrentLocation(fusedLocationClient: FusedLocationProviderClient) {
+        _locationLoading.value = true
+        _locationSuccess.value = false
+
+        val cancellationToken = CancellationTokenSource()
+
+        fusedLocationClient.getCurrentLocation(
+            Priority.PRIORITY_HIGH_ACCURACY,
+            cancellationToken.token
+        ).addOnSuccessListener { location ->
+            if (location != null) {
+                updateRequest(
+                    _request.value.copy(
+                        hospitalLat = location.latitude,
+                        hospitalLng = location.longitude
+                    )
+                )
+                _locationSuccess.value = true
+            } else {
+                _locationSuccess.value = false
+            }
+            _locationLoading.value = false
+        }.addOnFailureListener {
+            _locationSuccess.value = false
+            _locationLoading.value = false
+        }
     }
 
     fun publish(): Boolean {
@@ -33,7 +73,7 @@ class RequestViewModel : ViewModel() {
             return false
         }
 
-        // 3. لو كله تمام، حدث الوقت وانشر
+        // 3. لو كله تمام، حدث الوقت للرفع
         _request.value = current.copy(createdAt = System.currentTimeMillis())
         _error.value = null
         return true
