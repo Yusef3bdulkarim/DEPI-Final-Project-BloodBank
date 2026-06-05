474fadae619b289ece0d7437034461a0356115b4
Mustafa_Essam <mustafaessam2004@gmail.com>
2026-06-05 23:22:07 +0300

    feat(home): connect home screen with firestore to display real name and blood type

--- app/src/main/java/com/example/depi_final_project_bloodbank/ui/screens/home/HomeScreen.kt
+++ app/src/main/java/com/example/depi_final_project_bloodbank/ui/screens/home/HomeScreen.kt
@@ -3,8 +3,11 @@ package com.example.depi_final_project_bloodbank.ui.screens.home
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.lifecycle.viewmodel.compose.viewModel // متنساش تعمل Import للـ viewModel
 import com.example.depi_final_project_bloodbank.ui.screens.home.components.ActionButtonSection
 import com.example.depi_final_project_bloodbank.ui.screens.home.components.DonationCounterBanner
 import com.example.depi_final_project_bloodbank.ui.screens.home.components.DynamicHealthTipsSection
@@ -12,25 +15,30 @@ import com.example.depi_final_project_bloodbank.ui.screens.home.components.Heade
 import com.example.depi_final_project_bloodbank.ui.screens.home.components.SectionTitle
 import com.example.depi_final_project_bloodbank.ui.screens.home.components.TopLogoBar
 import com.example.depi_final_project_bloodbank.ui.screens.home.components.UrgentAppealsList
+import com.example.depi_final_project_bloodbank.ui.screens.home.viewmodel.HomeViewModel
 
 @Composable
 fun HomeScreen(
-    onRequestBloodClick: () -> Unit = {}
+    onRequestBloodClick: () -> Unit = {},
+    viewModel: HomeViewModel = viewModel() // 1. ضفنا الـ ViewModel هنا
 ) {
-    val userName = "Ahmed"
-    val bloodType = "O+"
+    // 2. بنراقب حالة الشاشة عشان نجيب منها البيانات الحقيقية
+    val uiState by viewModel.uiState.collectAsState()
+
+    // مسحنا القيم الثابتة بتاعت userName و bloodType من هنا
     val daysElapsed = 44
     val nextDate = "25/9/2026"
     val lastDate = "12/8/2026"
 
     LazyColumn(
         modifier = Modifier.fillMaxSize()
-        // الـ Padding هييجي من الـ Scaffold الأم اللي بره
     ) {
 
         item { TopLogoBar() }
 
-        item { HeaderSection(userName, bloodType) }
+        // 3. بصينا القيم الحقيقية من الـ uiState
+        item { HeaderSection(uiState.userName, uiState.bloodType) }
+
         item { DonationCounterBanner(daysElapsed, nextDate, lastDate) }
         item { DynamicHealthTipsSection() }
         item { ActionButtonSection(onRequestBloodClick = onRequestBloodClick) }
@@ -38,7 +46,6 @@ fun HomeScreen(
         item { UrgentAppealsList() }
     }
 }
-
 
 @Preview(showBackground = true)
 @Composable
