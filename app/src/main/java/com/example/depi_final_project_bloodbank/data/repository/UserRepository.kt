9bcb74dca6df0daaf846ccabf51b0ffeaf33cafe
Mustafa_Essam <mustafaessam2004@gmail.com>
2026-06-05 23:21:02 +0300

    feat(data): add UserRepository to fetch current user data from Firestore

--- app/src/main/java/com/example/depi_final_project_bloodbank/data/repository/UserRepository.kt
+++ app/src/main/java/com/example/depi_final_project_bloodbank/data/repository/UserRepository.kt
@@ -1,5 +1,24 @@
 package com.example.depi_final_project_bloodbank.data.repository
 
+import com.example.depi_final_project_bloodbank.domain.model.User
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.firestore.FirebaseFirestore
+import kotlinx.coroutines.tasks.await
+
 class UserRepository {
-    // TODO: Firebase Auth + Users Collection operations
+    private val auth = FirebaseAuth.getInstance()
+    private val firestore = FirebaseFirestore.getInstance()
+
+    // دالة بتروح تجيب بيانات المستخدم الحالي من فايربيز
+    suspend fun getCurrentUser(): User? {
+        val uid = auth.currentUser?.uid ?: return null
+        return try {
+            // بنجيب الدوكيومنت ونحوله مباشرة لكلاس User بتاعك
+            val document = firestore.collection("Users").document(uid).get().await()
+            document.toObject(User::class.java)
+        } catch (e: Exception) {
+            e.printStackTrace()
+            null
+        }
+    }
 }
